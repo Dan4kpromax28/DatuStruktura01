e76a8cc79b556e9fe1cded13aec7d8cd4894fbf7
Dan4kpromax28 <danikbalik@gmail.com>
2024-02-28 09:50:30 +0200

    Second

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,4 +1,44 @@
 package datastr;
 
 public class MyArrayList {
+
+    private int[] list;
+    private final int LIST_DEFAULT_SIZE = 10;//final ir constatnta
+    private int size = LIST_DEFAULT_SIZE;
+    private int counter = 0; // cik elementi ir
+
+    //TODO
+    //construktors
+    public MyArrayList(){
+        list = new int[size];
+    }
+    public MyArrayList(int inputSize){
+        if(inputSize > 0) {
+            size = inputSize;
+        }
+        list = new int[size];
+    }
+    //isEmpty
+    public boolean isEmpty(){
+        // 1. long if-else
+       /* if (counter == 0){
+            return true;
+        }else
+            return false;*/
+        //2. Short if-else
+        //return (counter == 0)? true : false;
+        //3.
+        return (counter==0);
+    }
+
+
+
+    // isFull
+    public boolean isFull(){
+        return (counter == size);
+    }
+    // howManyElements
+    public int howManyElements(){
+        return counter;
+    }
 }
